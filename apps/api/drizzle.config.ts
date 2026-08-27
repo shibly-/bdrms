cee2af375b8e4237217360d1cf295274d70ef149
ASM-WOKSTATION\Hp <shibly.dhk@gmail.com>
2026-08-27 07:38:10 +0600

    Removed neon db config

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
   out: "./drizzle",
   dialect: "postgresql",
   dbCredentials: {
-    //url: process.env.DATABASE_URL ?? "postgres://postgres:postgres@localhost:5432/bdrms",
-    url: "postgresql://neondb_owner:npg_KXYvV5T4cJqP@ep-hidden-heart-ax3k10vr-pooler.c-4.us-east-2.aws.neon.tech/bdrms_db",
+    url: process.env.DATABASE_URL ?? "postgres://postgres:postgres@localhost:5432/bdrms",    
   },
 });
