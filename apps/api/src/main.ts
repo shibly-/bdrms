e6660bd090fb62bc3a32de20d5b68b90e71ac225
ASM-WOKSTATION\Hp <shibly.dhk@gmail.com>
2026-08-28 01:14:23 +0600

    updated for vercel

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,7 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useBodyParser('json', { limit: '12mb' });
   app.useBodyParser('urlencoded', { extended: true, limit: '12mb' });
-  app.enableCors();
+  app.enableCors({
+    origin: [
+      'https://bdrms-web.vercel.app',   // production UI
+      'http://localhost:3000'         // dev UI
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true
+  });
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 4000);
 }
