97c0412d8a9af3ead162bf6feb52bfad435f0887
ASM-WOKSTATION\Hp <shibly.dhk@gmail.com>
2026-08-29 22:27:53 +0600

    Removed vercel config.

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,14 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useBodyParser('json', { limit: '12mb' });
   app.useBodyParser('urlencoded', { extended: true, limit: '12mb' });
-  app.enableCors({
-    origin: [
-      'https://bdrms-web.vercel.app',   // production UI
-      'http://localhost:3000'         // dev UI
-    ],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true
-  });
+  app.enableCors();
   app.setGlobalPrefix('api');
   await app.listen(process.env.PORT ?? 4000);
 }
